dcdb16e2c2a283421dc386f93adfcc86964aac1e
OliverFaxe <oliverfaxe@hotmail.com>
2024-10-25 18:23:28 +0200

    First update

---
+++ index.js
@@ -0,0 +1,11 @@
+alert("Välkommen till MiniCalculator");
+console.log("Välkommen!");
+
+let firstNumber = prompt("Slå in första siffran");
+
+let secondNumber = prompt("Slå in andra siffan");
+
+let math = prompt("Använd + - / * för att räkna ut");
+
+
+
